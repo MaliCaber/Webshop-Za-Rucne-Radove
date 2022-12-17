0eb99d5fab2209b62528afb629b9ab87347dd03e
Nikola <34938416+MaliCaber@users.noreply.github.com>
2022-12-17 17:18:06 +0100

    Delete artikal directory

--- artikal/context_processors.py
+++
@@ -1,6 +0,0 @@
-from artikal.models import Kategorija
-
-
-def menu_kategorije(request):
-    kategorije = Kategorija.objects.all()
-    return {'kategorije': kategorije}
--- artikal/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.forms.fields import IntegerField
-from django.forms.forms import Form
-
-
-class DodajUKorpuForm(forms.Form):
-    kolicina = forms.IntegerField()
