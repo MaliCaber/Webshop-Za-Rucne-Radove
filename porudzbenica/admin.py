4f6d30acc4d4ff47cb73595e9f1a0c1cc2aee22a
Nikola <34938416+MaliCaber@users.noreply.github.com>
2022-12-17 21:41:07 +0100

    Delete porudzbenica directory

--- porudzbenica/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Porudzbenica, PoruceniArtikal
-
-# Register your models here.
-admin.site.register(Porudzbenica)
-admin.site.register(PoruceniArtikal)
